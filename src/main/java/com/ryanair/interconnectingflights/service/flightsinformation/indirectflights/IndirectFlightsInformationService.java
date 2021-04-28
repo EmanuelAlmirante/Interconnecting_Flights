c239f25c428aa23dfc6dc52520093dfe90aefd17
Emanuel Almirante <emanuelalmirante@gmail.com>
2021-04-28 09:37:33 +0100

    Add indirect flights classes

--- src/main/java/com/ryanair/interconnectingflights/service/flightsinformation/indirectflights/IndirectFlightsInformationService.java
+++ src/main/java/com/ryanair/interconnectingflights/service/flightsinformation/indirectflights/IndirectFlightsInformationService.java
@@ -1,4 +1,13 @@
 package com.ryanair.interconnectingflights.service.flightsinformation.indirectflights;
 
+import com.ryanair.interconnectingflights.service.domain.FlightsInformation;
+
+import java.util.List;
+
 public interface IndirectFlightsInformationService {
+    FlightsInformation getIndirectFlightsInformation(String departure,
+                                                     String arrival,
+                                                     String departureDateTime,
+                                                     String arrivalDateTime,
+                                                     List<String> commonInterconnectingAirports);
 }
--- src/main/java/com/ryanair/interconnectingflights/service/flightsinformation/indirectflights/IndirectFlightsInformationServiceImpl.java
+++ src/main/java/com/ryanair/interconnectingflights/service/flightsinformation/indirectflights/IndirectFlightsInformationServiceImpl.java
@@ -1,7 +1,27 @@
 package com.ryanair.interconnectingflights.service.flightsinformation.indirectflights;
 
+import com.ryanair.interconnectingflights.provider.schedules.SchedulesProvider;
+import com.ryanair.interconnectingflights.service.domain.FlightsInformation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class IndirectFlightsInformationServiceImpl implements IndirectFlightsInformationService {
+    private final SchedulesProvider schedulesProvider;
+
+    @Autowired
+    public IndirectFlightsInformationServiceImpl(SchedulesProvider schedulesProvider) {
+        this.schedulesProvider = schedulesProvider;
+    }
+
+    @Override
+    public FlightsInformation getIndirectFlightsInformation(String departure,
+                                                            String arrival,
+                                                            String departureDateTime,
+                                                            String arrivalDateTime,
+                                                            List<String> commonInterconnectingAirports) {
+        return null;
+    }
 }
